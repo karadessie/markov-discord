77ebcdfb6d5798b591d6a8b32517b4acd1eef156
Kara Ballew <99826545+karadessie@users.noreply.github.com>
2022-07-05 18:18:32 -0400

    Add files via upload

--- markov.py
+++ markov.py
@@ -1,27 +1,24 @@
-"""A Markov chain generator that can tweet random messages."""
-
-import sys
 from random import choice
 
 
-def open_and_read_file(filenames):
-    """Take list of files. Open them, read them, and return one long string."""
+def open_and_read_file():
 
-    body = ''
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
+    file = open('green-eggs.txt')
+    text_string = file.read()
+    file.close()
 
-    return body
-
+    return text_string
 
 def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains."""
 
     chains = {}
 
     words = text_string.split()
+
+    # To set a stop point, append None to the end of our word list.
+
+    words.append(None)
+
     for i in range(len(words) - 2):
         key = (words[i], words[i + 1])
         value = words[i + 2]
@@ -35,32 +32,31 @@ def make_chains(text_string):
 
 
 def make_text(chains):
-    """Take dictionary of Markov chains; return random text."""
-
-    keys = list(chains.keys())
-    key = choice(keys)
-
+    
+    key = choice(list(chains.keys()))
     words = [key[0], key[1]]
-    while key in chains:
-        # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text).
+    word = choice(chains[key])
 
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
+    # Keep looping until we reach a value of None
+    # (which would mean it was the end of our original text)
+    # Note that for long texts (like a full book), this might mean
+    # it would run for a very long time.
 
-        word = choice(chains[key])
-        words.append(word)
+    while word is not None:
         key = (key[1], word)
+        words.append(word)
+        word = choice(chains[key])
 
     return ' '.join(words)
 
+def main():
 
-# Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
-filenames = sys.argv[1:]
+    text_string = open_and_read_file()
 
-# Open the files and turn them into one long string
-text = open_and_read_file(filenames)
+    chains = make_chains(text_string)
 
-# Get a Markov chain
-chains = make_chains(text)
+    random_text = make_text(chains)
+
+    print(random_text)
+
+main()
